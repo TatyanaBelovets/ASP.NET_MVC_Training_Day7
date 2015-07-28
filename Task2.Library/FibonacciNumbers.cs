ae742af5e8bbae18a32a4edc11eec469cc562220
TatyanaBelovets <Tatyana.Belovets@gmail.com>
2015-07-29 02:24:32 +0300

    Added GetFibonacciNumber method

---
+++ Task2.Library/FibonacciNumbers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2.Library
+{
+    public class FibonacciNumbers
+    {
+        public IEnumerable<int> GetFibonacciNumber()
+        {
+            int prev = 0;
+            int curr = 1;
+            while (true)
+            {
+                yield return curr;
+                curr += prev;
+                prev -= curr;
+            }
+        }
+    }
+}
