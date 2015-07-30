87ac09eb6ee685b2079484fcf3148fbdaa0ca3c0
TatyanaBelovets <Tatyana.Belovets@gmail.com>
2015-07-30 14:50:54 +0300

    Added CountdownClock

---
+++ Task1.Library/CountdownClock.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace Task1.Library
+{
+    public class CountdownClock
+    {
+        public delegate void TimeUpEventHandler();
+
+        public event TimeUpEventHandler TimeUp;
+
+        public void ImitationCountdownClock(int seconds)
+        {
+            Thread.Sleep(seconds * 1000);
+            Console.WriteLine("Time is up!");
+            if (TimeUp != null) TimeUp();
+        }
+    }
+
+    
+}
