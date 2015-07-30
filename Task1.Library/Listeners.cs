9754f95a6ba760cad5d0f7507dde297120be0051
TatyanaBelovets <Tatyana.Belovets@gmail.com>
2015-07-30 14:47:08 +0300

    Added class CountdownClock, Listeners and ConsoleCheck

---
+++ Task1.Library/Listeners.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Task1.Library
+{
+    public class Microwave
+    {
+        private static void MicrowaveMsg()
+        {
+            Console.WriteLine("Your breakfast is ready!");
+        }
+
+        public void Unregister(CountdownClock clock)
+        {
+            clock.TimeUp -= MicrowaveMsg;
+        }
+
+        public void Register(CountdownClock clock)
+        {
+            clock.TimeUp += MicrowaveMsg;
+        }
+    }
+
+    public class Bathroom
+    {
+        private static void BathroomMsg()
+        {
+            Console.WriteLine("Bathroom is ready for using!");
+        }
+
+        public void Unregister(CountdownClock clock)
+        {
+            clock.TimeUp -= BathroomMsg;
+        }
+
+        public void Register(CountdownClock clock)
+        {
+            clock.TimeUp += BathroomMsg;
+        }
+    }
+}
---
+++ Task1.TestConsole/CountdownClockCheck.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Task1.Library;
+
+namespace Task1.TestConsole
+{
+    class CountdownClockCheck
+    {
+        static void Main(string[] args)
+        {
+            var clock = new CountdownClock();
+            var bath = new Bathroom();
+            var microwave = new Microwave();
+            bath.Register(clock);
+            microwave.Register(clock);
+            Console.WriteLine("The countdown has begun!");
+            clock.ImitationCountdownClock(5);
+            bath.Unregister(clock);
+            clock.ImitationCountdownClock(2);
+            Console.ReadKey();
+        }
+    }
+}
