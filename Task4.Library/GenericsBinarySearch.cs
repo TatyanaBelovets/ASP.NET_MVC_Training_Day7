5d3d36e787c972a6c834b2fc252ab4f43eab0410
TatyanaBelovets <Tatyana.Belovets@gmail.com>
2015-07-30 11:31:46 +0300

    Added method wrapper without pointing left and right indexes; changed Comparison to IComparer 'cause it allows use default Comparer for built-in types

--- Task4.Library/GenericsBinarySearch.cs
+++ Task4.Library/GenericsBinarySearch.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,36 +9,41 @@ namespace Task4.Library
 {
     public static class GenericsBinarySearch
     {
-        public static int BinarySearch<T>(T[] array, T key, int left, int right, Comparison<T> compare  /*IComparer<T> comparer*/)
+        public static int BinarySearch<T>(T[] array, T key, IComparer<T> comparer)
+        {
+            return BinarySearch(array, key, 0, array.Length, comparer);
+        }
+
+        public static int BinarySearch<T>(T[] array, T key, int left, int right, IComparer<T> comparer)
         {
             if (array.Length == 0)
             {
                 throw new ArgumentException("Empty array", "array");
             } // or return -1 better?
-            if (compare(key, array[array.Length - 1]) > 0 || compare(key, array[0]) < 0)
+            if (comparer.Compare(key, array[array.Length - 1]) > 0 || comparer.Compare(key, array[0]) < 0)
             {
                 return -1;
             }
             if (left == right)
             {
-                if (compare(array[left], key) == 0)
+                if (comparer.Compare(array[left], key) == 0)
                 {
                     return left;
                 }
                 return -1;
             }
             int mid = left + (right - left)/2;
-            if (compare(key, array[mid]) == 0)
+            if (comparer.Compare(key, array[mid]) == 0)
             {
                 return mid;
             }
-            if (compare(key, array[mid]) > 0)
+            if (comparer.Compare(key, array[mid]) > 0)
             {
                 left = mid + 1;
-                return BinarySearch(array, key, left, right, compare);
+                return BinarySearch(array, key, left, right, comparer);
             }
             right = mid;
-            return BinarySearch(array, key, left, right, compare);
-        } 
+            return BinarySearch(array, key, left, right, comparer);
+        }
     }
 }
